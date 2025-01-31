186105d444c04be4b589a034e4d3351c19ec1d47
VictorEdsonsr <edvictorsr@gmail.com>
2025-01-31 13:53:34 -0300

    finished project

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ export default function index() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                    <a href="https://github.com/VictorEdsonsr" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <img src={GithubIcon} alt="Github" />
                             <p className="dsmovie-contact-link">/VictorEdsonsr</p>
