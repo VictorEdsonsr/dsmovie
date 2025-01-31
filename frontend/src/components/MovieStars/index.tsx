d61ddb85a3c815fd1b8de20fae380c580cc135c5
VictorEdsonsr <edvictorsr@gmail.com>
2025-01-31 13:05:34 -0300

    add request, pagination and others

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -3,14 +3,53 @@ import StarHalf from "../../assets/star-half.svg";
 import StarEmpty from "../../assets/star-empty.svg";
 import "./styles.css";
 
-export default function MovieStars() {
+type Props = {
+    score: number;
+}
+
+type StarProps = {
+    fill: number;
+}
+
+function Stars({ fill }: StarProps) {
+    if (fill === 1) {
+        return <img src={StarFull} alt="star full" />;
+    } else if (fill === 0) {
+        return <img src={StarEmpty} alt="star empty" />;
+    } else {
+        return <img src={StarHalf} alt="star half" />
+    }
+}
+
+
+export default function MovieStars({ score }: Props) {
+    function getFills(score: number) {
+
+        const fills = [0, 0, 0, 0, 0];
+
+        const integerPart = Math.floor(score);
+
+        for (let i = 0; i < integerPart; i++) {
+            fills[i] = 1;
+        }
+
+        const diff = score - integerPart;
+        if (diff > 0) {
+            fills[integerPart] = 0.5;
+        }
+
+        return fills;
+    }
+
+    const fills = getFills(score);
+
     return (
         <div className="dsmovie-stars-container">
-            <img src={StarFull} />
-            <img src={StarFull} />
-            <img src={StarFull} />
-            <img src={StarHalf} />
-            <img src={StarEmpty} />
+            <Stars fill={fills[0]} />
+            <Stars fill={fills[1]} />
+            <Stars fill={fills[2]} />
+            <Stars fill={fills[3]} />
+            <Stars fill={fills[4]} />
         </div>
     )
 }
--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,15 +1,29 @@
 import "./styles.css"
 import Arrow from "../../assets/arrow.svg"
+import { MoviePage } from "../../types/movies";
 
-export default function Pagination() {
+type Props = {
+    page: MoviePage;
+    onChange: Function
+}
+
+export default function Pagination({ page, onChange }: Props) {
     return (
         <div className="dsmovie-pagination-container">
             <div className="dsmovie-pagination-box">
-                <button className="dsmovie-pagination-button" disabled={true} >
+                <button
+                    className="dsmovie-pagination-button"
+                    disabled={page?.first}
+                    onClick={() => onChange(page.number - 1)}
+                >
                     <img src={Arrow} className="dsmovie-flip-horizontal" />
                 </button>
-                <p>{`${1} de ${3}`}</p>
-                <button className="dsmovie-pagination-button" disabled={false} >
+                <p>{`${page?.number + 1} de ${page?.totalPages}`}</p>
+                <button
+                    className="dsmovie-pagination-button"
+                    disabled={page?.last}
+                    onClick={() => onChange(page.number + 1)}
+                >
                     <img src={Arrow} />
                 </button>
             </div>
